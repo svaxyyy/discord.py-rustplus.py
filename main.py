941891cf6e0a8e0fdbe6741c42227e2bab097809
svaxyyy <69980766+svaxyyy@users.noreply.github.com>
2021-08-15 00:12:12 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,220 @@
+from discord.embeds import Embed
+from discord.enums import Enum
+from rustplus import RustSocket
+import json
+import discord
+from discord.ext import commands
+import asyncio
+
+with open("database/json/fcm-listen-output.json", "r")as file:
+    output = json.load(file)
+
+
+
+
+
+_IP = str(output["ip"])
+_PORT = str(output["port"])
+_STEAMID = int(output["playerId"])
+_PLAYERTOKEN = int(output["playerToken"])
+
+
+
+intents = discord.Intents.default()
+intents.members = True
+
+
+color = 0xffffff
+
+# json
+
+
+
+socket = RustSocket(_IP, _PORT, _STEAMID, _PLAYERTOKEN)
+_CONNECT = socket.connect() 
+async def connect():
+    socket.connect().print("RustSocket connected!")
+
+async def disconnect():
+    socket.disconnect().print("RustSocket disconnected!")
+
+
+
+
+def load():
+    with open("database/json/bot_config.json", "r") as file:
+        return json.load(file)
+
+data = load()
+
+client = commands.Bot(command_prefix=data["prefix"], intents=intents)
+
+_fasterMap = True
+_recreatedTeamEvent = True
+
+
+@client.event
+async def on_ready():
+
+    print('We have logged in as {0.user}'.format(client))
+    _CONNECT
+    team_info = socket.getTeamInfo()
+
+
+    ## Or, to get all the names in an array
+    await asyncio.create_task(recreatedTeamEvent())
+
+
+
+    await startAll()
+
+
+
+
+async def startAll():
+    if _fasterMap:
+        print("started: fasterMap")
+        await asyncio.create_task(fasterMap())
+    if _recreatedTeamEvent:
+        print("started: recreatedTeamEvent")
+        await asyncio.create_task(recreatedTeamEvent(), fasterMap())
+        
+        
+
+
+async def runningEvents(ctx):
+    _LOOP = True
+
+    while _LOOP:
+        await asyncio.sleep(60)
+        if events:
+            embed = discord.Embed(title="These event are Running right now:")
+            for event in events:
+                embed.add_field(name="Event:", value=event.name)
+            
+            await ctx.send(embed=embed)
+
+        else:
+            embed = discord.Embed(title="There are no events running right now!")
+            await ctx.send(embed=embed)
+
+
+async def fasterMap():
+    while True:
+        socket.getMap(addIcons = True, addEvents = True, addVendingMachines= True).save("RustMap.PNG")
+        await asyncio.sleep(60)
+
+
+@client.command()
+async def map(ctx):
+    file = discord.File('RustMap.PNG')
+    await ctx.send(file=file)
+    
+@client.command()
+async def events(ctx):
+    events = socket.getCurrentEvents()
+    if events:
+        embed = discord.Embed(title="These event are Running right now:")
+        for event in events:
+            embed.add_field(name="Event:", value=event.type.value)
+        
+        await ctx.send(embed=embed)
+
+    else:
+        embed = discord.Embed(title="There are no events running right now!")
+        await ctx.send(embed=embed)
+
+
+@client.command()
+async def pop(ctx):
+    dict = socket.getInfo()
+    embed=Embed(title="Server Pop", description="Pop: " + "`" + str(dict['currentPlayers']) + "/" + str(dict['maxPlayers']) + "`" + "\n\nQueue: " + "`" + str(dict['queuedPlayers']) + "`" + "\n\nServer: " + "`" + str(dict['name']) + "`")
+    await ctx.send(embed=embed)
+
+@client.command()
+async def team(ctx):
+    with open("database/json/rust-msg.json", "r") as file:
+        data = json.load(file)
+    team = socket.getTeamInfo()
+    memberNames = []
+    guild = ctx.guild
+    msg = await ctx.send("Refreshes every `60` seconds")
+
+    if not guild.id in data:
+        data[str(guild.id)] = {}
+        data[str(guild.id)]["team-channel-id"] = ctx.channel.id
+        data[str(guild.id)]["team-msg-id"] = msg.id
+
+    data[str(guild.id)]["team-channel-id"] = ctx.channel.id
+    data[str(guild.id)]["team-msg-id"] = msg.id
+
+    with open("database/json/rust-msg.json", "w") as file:
+        data = json.dump(data,file,indent=4)
+    def getLeaderName():
+        for member in team.members:
+            if member.steamId == team.leaderSteamId:
+                return member.name
+
+    embed=Embed(title="Team Info", description=f"Leader steam id: `{getLeaderName()}`")
+
+
+
+
+    for member in team.members:
+        memberNames.append(member.name)
+
+        embed.add_field(name=member.name, value=f'steam-id: `{member.steamId}` \n\nStatus: {("`🟢 Online`" if member.isOnline else "`🔴 Offline`")}\n\nSpawntime: `{member.spawnTime}`\n\nis alive: `{("`✅ alive`" if member.isAlive else f"`☠️ Death ({member.deathTime} seconds)`")}`')
+
+    await msg.edit(embed=embed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async def recreatedTeamEvent():
+
+    while True:
+        
+        with open("database/json/rust-msg.json", "r") as file:
+            data = json.load(file)
+        team = socket.getTeamInfo()
+        
+        for guild in client.guilds:
+
+            memberNames = []
+            teamMsgId = data[str(guild.id)]["team-msg-id"]
+            teamChannelId = data[str(guild.id)]["team-channel-id"]
+            mChannel = client.get_channel(teamChannelId)
+            mMsg = await mChannel.fetch_message(teamMsgId)
+
+            def getLeaderName():
+                for member in team.members:
+                    if member.steamId == team.leaderSteamId:
+                        return member.name
+
+            embed=Embed(title="Team Info", description=f"Leader Name: `{getLeaderName()}`")
+
+            for member in team.members:
+                memberNames.append(member.name)
+
+                embed.add_field(name=member.name, value=f'steam-id: `{member.steamId}` \n\nStatus: {("`🟢 Online`" if member.isOnline else "`🔴 Offline`")}\n\nSpawntime: `{member.spawnTime}`\n\nis alive: `{("`✅ alive`" if member.isAlive else f"`☠️ Death ({member.deathTime} seconds)`")}`')
+
+            await mMsg.edit(embed=embed)
+            print("recreatedTeamEvent() ran!")
+            await asyncio.sleep(60)
+
+
+
+client.run(data["token"])
